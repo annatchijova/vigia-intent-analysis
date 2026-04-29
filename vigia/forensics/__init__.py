6a749a3767a36a52c05c1826763fa018c442d663
Anna Tchijova <anna.tchijova@gmail.com>
2026-04-29 03:14:58 -0300

    license: cambiar a Apache 2.0 para SANS submission — headers AGPL removidos

--- vigia/forensics/__init__.py
+++ vigia/forensics/__init__.py
@@ -1,9 +1,3 @@
-# Copyright (c) 2026 Anna Tchijova
-# Vigía - Autonomous Incident Response Engine
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of the
 # License, or (at your option) any later version.
 
 """vigia.forensics — Analisis forense de artefactos."""
